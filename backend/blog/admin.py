ea33caedc4cdddb42b321d19425adc2f7bc6c90f
mhkmcp <mhkmcp@yahoo.com>
2021-09-20 20:03:56 +0600

    1-004: views & serializers

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post, Category
 
-# Register your models here.
+
+@admin.register(Post)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'excerpt', 'content', 'author', 'status')
+    prepopulated_fields = {'slug': ('title',),}
+
+
+admin.site.register(Category)
