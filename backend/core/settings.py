455e94e92ba033d3f181011eb9b94d101afeb13e
mhkmcp <mhkmcp@yahoo.com>
2021-09-20 20:16:34 +0600

    1-006: project level permission

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -125,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# project level permission
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
