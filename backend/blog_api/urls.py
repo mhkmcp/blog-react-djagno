cd693417bdb5d6d8b4867a5bc49d2533bdc4c6d6
mhkmcp <mhkmcp@yahoo.com>
2021-09-20 18:37:16 +0600

    1-002: tested with url

---
+++ backend/blog_api/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+# from .views import PostList, PostDetail
+
+app_name = 'blog_api'
+
+urlpatterns = [
+    # path('<int:pk>', PostDetail.as_view(), name='create-detail'),
+    # path('', PostList.as_view(), name='create-list'),
+]
